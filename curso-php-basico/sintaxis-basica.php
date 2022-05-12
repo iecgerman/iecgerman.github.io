faec68edce774822c0ef4554dcf52d07ff9be229
GERMAN GARCIA <iec_german@hotmail.com>
2022-05-12 01:23:37 -0500

    variables y tipos de datos

---
+++ curso-php-basico/sintaxis-basica.php
@@ -0,0 +1,35 @@
+<?php
+
+echo "Estoy desde la carpeta de /var/www/html usando la terminal de WSL \n";
+
+$nombre = "Germán";
+$apellido = "García";
+
+echo "No, no. Yo me llamo " . $nombre . " " . $apellido . " \n";
+echo "No, no. Yo me llamo $nombre $apellido \n";
+
+echo "el resultado de 4 x 5 es " . (4 * 5) . "\n";
+
+$personas = [
+      "Carlos" => 22,
+      "Mr. Michi" => 15,
+      "Juan" => 65
+];
+
+var_dump( $personas );
+
+print_r( $personas );
+
+// Esto es un comentario
+
+# Esto es otro comentario
+
+/*
+es otra forma de comentar
+pero con varios renglones
+😎
+*/
+
+
+
+echo "\n";
---
+++ curso-php-basico/variables.php
@@ -0,0 +1,16 @@
+<?php
+
+$numero_1 = 8;
+
+$numero_2 = 7;
+
+echo $numero_1 + $numero_2;
+
+define("NUMERO_PI", 3.14);
+
+echo "\n";
+
+echo NUMERO_PI;
+
+echo "\n";
+
